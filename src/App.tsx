6628189768866820c0e7a0d309eceae797749515
Justin Martinez <justinmartinez14@gmail.com>
2021-06-26 16:52:22 -0600

    removes route dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch} from 'react-router-dom'
+import { Router, Switch} from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import history from './utils/history'
