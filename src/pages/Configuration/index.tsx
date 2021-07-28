887fdc8757375a73a22293d4d77938a70ed22853
Justin Martinez <justinmartinez14@gmail.com>
2021-07-27 21:38:57 -0600

    Added skeleton of trial config

--- src/pages/Configuration/index.tsx
+++ src/pages/Configuration/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import ConfigureTrial from '../../components/ConfigureTrial'
 
 const Configuration = () => {
 
   const params = useParams()
   console.log(params)
   return (
-    <div>Configuration page</div>
+    <ConfigureTrial />
   )
 }
 
