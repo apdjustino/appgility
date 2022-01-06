c5aa034094ceb3d6f55ee73b49b8b6cb9ce1835b
Justin Martinez <justinmartinez14@gmail.com>
2022-01-05 22:53:49 -0700

    add field array for pricing

--- src/links/ExpiredToken.ts
+++ src/links/ExpiredToken.ts
@@ -3,8 +3,8 @@ import history from "../utils/history";
 
 export const ExpiredTokenLink = new ApolloLink((operation, forward) => {
   return forward(operation).map((responseData) => {
-    const { data, errors } = responseData;
-    if (!!errors && errors[0].message === "jwt expired") {
+    const { data, errors } = responseData;    
+    if (!!errors && !!errors[0].extensions && errors[0].extensions.code === "UNAUTHENTICATED") {      
       localStorage.removeItem("appgilityAccessToken");
       window.location.pathname = "/"
     }
